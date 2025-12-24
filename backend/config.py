07d23a85acc7fb64a6809a165e785ee0b8e4d649
sial-sanwal <sialsanwal885@gmail.com>
2025-12-24 23:08:43 +0500

    added the GROQ API

--- backend/config.py
+++ backend/config.py
@@ -2,4 +2,5 @@ TWILIO_ACCOUNT_SID = ""
 TWILIO_AUTH_TOKEN = ""
 TWILIO_FROM_NUMBER = ""  # your Twilio number
 EMERGENCY_CONTACT = ""  # or your local emergency number
-OPENAI_API_KEY=""
+#OPENAI_API_KEY=""
+GROQ_API_KEY=
