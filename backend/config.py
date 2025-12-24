e3a719cbad1e217de9702e32d51ea75930efe890
sial-sanwal <sialsanwal885@gmail.com>
2025-12-24 23:26:57 +0500

    added the proper config file and load the .env

--- backend/config.py
+++ backend/config.py
@@ -1,6 +1,12 @@
-TWILIO_ACCOUNT_SID = ""
-TWILIO_AUTH_TOKEN = ""
-TWILIO_FROM_NUMBER = ""  # your Twilio number
-EMERGENCY_CONTACT = ""  # or your local emergency number
-#OPENAI_API_KEY=""
-GROQ_API_KEY=
+import os
+from dotenv import load_dotenv
+
+
+load_dotenv()
+
+twilio_sid = os.getenv("TWILIO_ACCOUNT_SID")
+twilio_token = os.getenv("TWILIO_AUTH_TOKEN")
+from_number = os.getenv("TWILIO_FROM_NUMBER")
+emergency_number = os.getenv("EMERGENCY_CONTACT")
+groq_key = os.getenv("GROQ_API_KEY")
+
